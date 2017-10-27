0e249baec18a6d83b7b50ed0b8b3fe1866fae75d
Michael Kelly <kclub39@gmail.com>
2017-10-27 12:47:11 -0600

    pushing problem 1

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,32 +1,37 @@
 
-/**
+/** Created by Michael on 10/26/2017.
  * Euler Project Problem 1
  * Multiples of 3 and 5
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
  *
- * Created by Michael on 10/26/2017.
+ * Correct answer found on 10/27/2017
  *
- *
+ * After the correct answer was found, the answer was generalised to any number
  */
 public class Problem1 {
     public static void main(String[] args) {
-    int SumOfRunningTotals = 0;
-    int multiplier=1;
-    while(multiplier*3 < 1000){
-        if ((multiplier%15) == 0 ){
-            SumOfRunningTotals =  (3*multiplier) + SumOfRunningTotals;
-        }
-        else if (multiplier*5 < 1000){
-            SumOfRunningTotals = 5*multiplier +3*multiplier + SumOfRunningTotals;
-        }
-        else if (multiplier*5 > 1000){
-            SumOfRunningTotals = 3*multiplier + SumOfRunningTotals;
-        }
-        multiplier = multiplier+1;
-        }
-        System.out.println(" The total is " + SumOfRunningTotals);
+        double realTotal;
+        int maxNumber = 1000;
+        int maxForThree, maxForFive, maxForFifteen;
 
+        maxForThree = maxNumber / 3;
+        if (maxNumber%3 ==0){
+            maxForThree--;
+        }
+        maxForFive = (maxNumber / 5);
+        if (maxNumber%5 ==0){
+            maxForFive--;
+        }
+        maxForFifteen = maxNumber / 15;
+        if (maxNumber%15 ==0){
+            maxForFifteen--;
+        }
+
+        realTotal = 5 * (.5 * maxForFive * (maxForFive + 1)) + 3 * (.5 * maxForThree * (maxForThree + 1)) -
+                15 * (.5 * (maxForFifteen * (maxForFifteen + 1)));
+
+        System.out.println(" the total is " + realTotal);
     }
 }
