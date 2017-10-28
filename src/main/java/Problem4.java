b3c4ece31a44378c9f18ce5d10d9b2b52b66ad3b
Michael Kelly <kclub39@gmail.com>
2017-10-28 08:28:12 -0600

    pushing problem 4

--- src/main/java/Problem4.java
+++ src/main/java/Problem4.java
@@ -1,8 +1,30 @@
 /**
  * Created by Michael on 10/27/2017.
+ * A palindromic number reads the same both ways.
+ * The largest palindrome made from the product of two 2-digit numbers is
+ * 9009 = 91 × 99.
+ *Find the largest palindrome made from the product of two 3-digit numbers.
  */
 public class Problem4 {
     public static void main(String[] args) {
+        int desiredpalindrom = 998001;
+        int totalMultiple;
+        int firstMultiplier=999;
+        int secondMultiplier =999;
 
+//        while(firstMultiplier > 100){
+//            while (secondMultiplier > 100){
+//                totalMultiple = firstMultiplier*secondMultiplier;
+//                int firstDigit = Integer.parseInt(Integer.toString(totalMultiple).substring(0, 1));
+//                System.out.println(" The first digit is " + firstDigit);
+//                System.out.println(" Current total " + totalMultiple);
+//                secondMultiplier--;
+//            }
+//            firstMultiplier--;
+//        }
+        int tester =12345;
+        while (tester.)
+        int firstDigit = Integer.parseInt(Integer.toString(tester).substring(0, 1));
+//                System.out.println(" The first digit is " + firstDigit);
     }
 }
