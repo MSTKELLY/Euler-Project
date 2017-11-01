3755663142a278d87b577a6ca297542aff51f7e0
Michael Kelly <kclub39@gmail.com>
2017-11-01 08:52:01 -0600

    placeholder for the 9th and 10th problems

---
+++ src/main/java/Problem10.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Michael on 11/1/2017.
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ *
+ */
+public class Problem10 {
+    public static void main(String[] args) {
+        System.out.println("placeholder");
+    }
+}
---
+++ src/main/java/Problem9.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Michael on 11/1/2017.
+ *
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ *
+ *a^2 + b^2 = c^2
+ *For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ *
+ *There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ *Find the product abc.
+ *
+ */
+public class Problem9 {
+    public static void main(String[] args) {
+        System.out.println("placeholder");
+    }
+}
