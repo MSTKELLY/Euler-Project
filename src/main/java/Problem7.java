f836885ce2f445fcbc774975509e08f0b575fcc2
Michael Kelly <kclub39@gmail.com>
2017-11-01 08:44:42 -0600

    placeholder for the 6th-8th problems

---
+++ src/main/java/Problem7.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Michael on 11/1/2017.
+ * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+ *
+ * What is the 10 001st prime number?
+ */
+public class Problem7 {
+    public static void main(String[] args) {
+        System.out.println("placeholder");
+    }
+}
