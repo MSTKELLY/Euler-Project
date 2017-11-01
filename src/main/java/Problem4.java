e346ad4e2457c9b0831afec296862db1bcbacd8a
Michael Kelly <kclub39@gmail.com>
2017-11-01 08:28:51 -0600

    Finished the 4th problem

--- src/main/java/Problem4.java
+++ src/main/java/Problem4.java
@@ -4,50 +4,66 @@
  * The largest palindrome made from the product of two 2-digit numbers is
  * 9009 = 91 × 99.
  *Find the largest palindrome made from the product of two 3-digit numbers.
+ *
+ * Finished on 10/31/2017
  */
+
 public class Problem4 {
     public static void main(String[] args) {
-        int totalMultiple, workingNumber, tester, palindrome=0;
-        int firstMultiplier=999, secondMultiplier =999, palindromeTest;
-        int[] digitsOfTesterArray = new int[5];
+        boolean palindrome;
+        int firstMultiplier=999, secondMultiplier =999;
+        int totalMultiple;
+        int largestPalindrome=0;
 
         while(firstMultiplier > 100){
+                secondMultiplier = 999;
+
             while (secondMultiplier > 100){
                 totalMultiple = firstMultiplier*secondMultiplier;
-                if (totalMultiple > 100000) {
-                    palindromeTest =100000;
-                    workingNumber = totalMultiple;
-                    while(palindromeTest > 0){
-                        tester = workingNumber/palindromeTest;
-                    //    System.out.println(tester);
-                        workingNumber =workingNumber-(tester*palindromeTest);
-                        palindromeTest = palindromeTest/10;
-                    }
-                   // System.out.println(totalMultiple);
-                    int i=0;
-                    int j=5;
-                    int k=0;
-                    System.out.println(digitsOfTesterArray[i]);
+                palindrome = IsPalindrome(totalMultiple);
 
-                    while(i >=3){
-                        if (digitsOfTesterArray[i] == digitsOfTesterArray[j]){
-                            k++;
-                        }
-                        i++;
-                        j--;
+                if(palindrome == true){
+                    if (totalMultiple >largestPalindrome){
+                        largestPalindrome =totalMultiple;
                     }
-                    if(k == 3){
-                        if(totalMultiple>palindrome ){
-                            palindrome= totalMultiple;
-                        }
-                    }
-
                 }
+
                 secondMultiplier--;
             }
+
             firstMultiplier--;
         }
-        System.out.println(" the largest is " +palindrome);
+        System.out.println(largestPalindrome);
 
+
+    }
+
+    public static boolean IsPalindrome(int totalMultiple){
+        int workingNumber;
+        int palindromeTest;
+        int tester;
+        int[] digitsOfTesterArray = new int[6];
+
+        workingNumber = totalMultiple;
+        palindromeTest = 100000;
+        int i=0;
+        while(i < 6){
+            tester = workingNumber/palindromeTest;
+            workingNumber = workingNumber-(tester*palindromeTest);
+            digitsOfTesterArray[i] = tester;
+            palindromeTest = palindromeTest/10;
+            i++;
+        }
+        int j = 0;
+        int k = 5;
+        int l = 0;
+        while(j < 3){
+            if(digitsOfTesterArray[j] == digitsOfTesterArray[k]){
+                l++;
+            }
+            k--;
+            j++;
+        }
+        return l == 3;
     }
 }
