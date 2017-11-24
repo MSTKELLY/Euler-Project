8b1681de9ff2eb7e70b3622ad8294cf550219f16
Michael Kelly <kclub39@gmail.com>
2017-11-24 14:15:33 -0700

    saving place

--- src/main/java/Problem5.java
+++ src/main/java/Problem5.java
@@ -1,12 +1,18 @@
+import jdk.nashorn.internal.ir.WhileNode;
+
 /**
  * Created by Michael on 11/1/2017.
  *
  *2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
  *What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  *
+ * 11/1/2017
+ * There is likely an easier way to perform this question
  */
 public class Problem5 {
     public static void main(String[] args) {
-        System.out.println("placeholder");
+
     }
+
+
 }
